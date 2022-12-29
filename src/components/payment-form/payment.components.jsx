442c73b2ab48f9b2ac6903c912c761e88be49ffe
irorochad <wisdomchadere@gmail.com>
2022-12-30 00:51:20 +0100

    installed netlify locally and triggered the first handshake request when a post request is made

--- src/components/payment-form/payment.components.jsx
+++ src/components/payment-form/payment.components.jsx
@@ -1,13 +1,40 @@
-import {CardElement} from '@stripe/react-stripe-js';
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { PaymentFormContainer, FormContainer } from "./payment.styles";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => {
+      return res.json();
+    });
+
+    console.log(response);
+  };
+
   return (
-    <div>
-       <CardElement />
-      <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay NOW</Button>
-    </div>
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment:</h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay NOW</Button>
+      </FormContainer>
+    </PaymentFormContainer>
   );
 };
 
